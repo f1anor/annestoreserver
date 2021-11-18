570447b0c9d355ccf9f71bcf876f4c8792f80f68
Sergey Kolesnik <f1anor@yandex.ru>
2021-11-18 14:44:50 +0300

    make cors

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ const sessions = require("./libs/sessions");
 const cors = require("cors");
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+    // origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+    origin: ["https://f1anor.github.io"],
     credentials: true,
   })
 );
