f9c539ba2972cddd943c4de94ad35a36e4b9928f
Sergey Kolesnik <f1anor@yandex.ru>
2021-11-25 22:11:00 +0300

    sessions17

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const session = require("express-session");
 const app = express();
-app.set("trust proxy");
+app.set("trust proxy", 1);
 
 const MongoStore = require("connect-mongo")(session);
 
