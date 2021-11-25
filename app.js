b09d608c52847bdfc4330abcead5bf1a9397b380
Sergey Kolesnik <f1anor@yandex.ru>
2021-11-25 21:20:36 +0300

    sessions8

--- app.js
+++ app.js
@@ -30,6 +30,17 @@ app.use(
   })
 );
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
+
 // Отдаем статику
 
 app.use(express.static("public"));
